b278037b7cf978f416a4e5f5d8876c562a0c79b2
Phyo <phyoakl@hexcode.tech>
2023-05-29 20:36:19 +0630

    added welcome messages for inprogress pages

--- src/routes/(authed)/backlogs/details/[slug]/+page.svelte
+++ src/routes/(authed)/backlogs/details/[slug]/+page.svelte
@@ -1,18 +1,20 @@
-<h1 class="h1">
-	<span
-		class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone"
-		>Here , We will display
-	</span>
-</h1>
-<h1 class="h1">
-	<span
-		class="bg-gradient-to-br from-red-500 to-yellow-500 bg-clip-text text-transparent box-decoration-clone"
-		>Detail of Backlog Slug</span
-	>
-</h1>
-<h1 class="h1">
-	<span
-		class="bg-gradient-to-br from-pink-500 to-violet-500 bg-clip-text text-transparent box-decoration-clone"
-		>And its history</span
-	>
-</h1>
+<div class="container h-full mx-auto flex flex-col justify-center items-center">
+	<h1 class="h1">
+		<span
+			class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone"
+			>Here , We will display
+		</span>
+	</h1>
+	<h1 class="h1">
+		<span
+			class="bg-gradient-to-br from-red-500 to-yellow-500 bg-clip-text text-transparent box-decoration-clone"
+			>Detail of Backlog Slug</span
+		>
+	</h1>
+	<h1 class="h1">
+		<span
+			class="bg-gradient-to-br from-pink-500 to-violet-500 bg-clip-text text-transparent box-decoration-clone"
+			>And its history</span
+		>
+	</h1>
+</div>
--- src/routes/(authed)/reports/+page.svelte
+++ src/routes/(authed)/reports/+page.svelte
@@ -1,18 +1,20 @@
-<h1 class="h1">
-	<span
-		class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone"
-		>Here , We display
-	</span>
-</h1>
-<h1 class="h1">
-	<span
-		class="bg-gradient-to-br from-red-500 to-yellow-500 bg-clip-text text-transparent box-decoration-clone"
-		>Report of each teams
-	</span>
-</h1>
-<h1 class="h1">
-	<span
-		class="bg-gradient-to-br from-pink-500 to-violet-500 bg-clip-text text-transparent box-decoration-clone"
-		>Team members , and projects</span
-	>
-</h1>
+<div class="container h-full mx-auto flex flex-col justify-center items-center">
+	<h1 class="h1">
+		<span
+			class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone"
+			>Here , We display
+		</span>
+	</h1>
+	<h1 class="h1">
+		<span
+			class="bg-gradient-to-br from-red-500 to-yellow-500 bg-clip-text text-transparent box-decoration-clone"
+			>Report of each teams
+		</span>
+	</h1>
+	<h1 class="h1">
+		<span
+			class="bg-gradient-to-br from-pink-500 to-violet-500 bg-clip-text text-transparent box-decoration-clone"
+			>Team members , and projects</span
+		>
+	</h1>
+</div>
