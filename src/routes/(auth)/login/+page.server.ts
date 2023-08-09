909b559bd8acf5894a5d5599abb6282ccec8ec8a
Phyo <phyoakl@hexcode.tech>
2023-08-09 14:14:15 +0630

    tweak: text color

--- src/routes/(auth)/login/+page.server.ts
+++ src/routes/(auth)/login/+page.server.ts
@@ -1,37 +1,49 @@
-import type { Actions } from './$types';
-import { redirect, fail } from '@sveltejs/kit';
-import { loginUser } from '$lib/api/auth';
-import { env } from '$env/dynamic/public';
+import type { Actions } from './$types'
+import { redirect, fail } from '@sveltejs/kit'
+import { loginUser } from '$lib/api/auth'
+import { env } from '$env/dynamic/public'
 
 export const actions: Actions = {
-	default: async (event) => {
-		const formData = Object.fromEntries(await event.request.formData());
+  default: async (event) => {
+    const formData = Object.fromEntries(await event.request.formData())
 
-		if (!formData.email || !formData.password) {
-			return fail(400, {
-				error: 'Missing email or password'
-			});
-		}
+    if (!formData.email || !formData.password) {
+      return fail(400, {
+        error: 'Missing email or password'
+      })
+    }
 
-		const { email, password } = formData as { email: string; password: string };
-		try {
-			const token = (await loginUser(email, password)).access_token;
-			event.cookies.set('token', `Bearer ${token}`, {
-				httpOnly: true,
-				path: '/',
-				secure: true,
-				sameSite: 'strict',
-				maxAge: 60 * 60 * 24, // 1 day
-				domain: env.PUBLIC_COOKIE_DOMAIN
-			});
-		} catch (error: any) {
-			return fail(400, {
-				error: error.message
-			});
-		}
+    const { email, password } = formData as { email: string; password: string }
+    try {
+      const token = (await loginUser(email, password)).access_token
+      if (env.PUBLIC_COOKIE_DOMAIN) {
 
-		// Set the cookie
+        event.cookies.set('token', `Bearer ${token}`, {
+          httpOnly: true,
+          path: '/',
+          secure: true,
+          sameSite: 'strict',
+          maxAge: 60 * 60 * 2400, // 100 day
+          // domain: env.PUBLIC_COOKIE_DOMAIN
+        })
+      } else {
+        event.cookies.set('token', `Bearer ${token}`, {
+          httpOnly: true,
+          path: '/',
+          secure: true,
+          sameSite: 'strict',
+          maxAge: 60 * 60 * 2400, // 100 day
+        })
+      }
 
-		throw redirect(302, '/');
-	}
-};
+    } catch (error: any) {
+      return fail(400, {
+        error: error.message
+      })
+    }
+
+    // Set the cookie
+
+    throw redirect(302, '/')
+  }
+}
